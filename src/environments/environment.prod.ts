005e70d60aef93ea78a158e3cfdbbfd45f5897a1
Alexis Ramos <alexisramoscaballero@gmail.com>
2020-06-14 01:49:19 -0500

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url_api: 'http://platzi-store.herokuapp.com/products',
+  url_api: 'https://platzi-store.herokuapp.com/products',
   firebaseConfig: {
     apiKey: 'AIzaSyBwcWAzqam6UyosOfZd3UQPJDJfCdehHcQ',
     authDomain: 'platzi-store-5bae6.firebaseapp.com',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url_api: 'http://platzi-store.herokuapp.com/products',
+  url_api: 'https://platzi-store.herokuapp.com/products',
   firebaseConfig: {
     apiKey: 'AIzaSyBwcWAzqam6UyosOfZd3UQPJDJfCdehHcQ',
     authDomain: 'platzi-store-5bae6.firebaseapp.com',
